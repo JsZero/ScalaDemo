a4b120b6bb88f9ae40035f74a7e445e6557b99b5
Waitomo <34368246+Waitomo@users.noreply.github.com>
2018-06-23 16:12:18 +0800

    commit message

--- src/main/scala/cn/neu/demo/FunctionDemo.scala
+++ src/main/scala/cn/neu/demo/FunctionDemo.scala
@@ -2,7 +2,8 @@ package cn.neu.demo
 
 object FunctionDemo {
   def main(args: Array[String]): Unit = {
-    // 在Scala中存在函数（function）和方法（method），函数和方法是不同的存在，区别如下所示：
+    // 在Scala中存在函数（function）和方法（method），函数和方法是不同的存在，方法可以说是类的一部分，而函数是一个对象
+    // 详细的区别如下所示：
     // 1. 方法不能作为单独的表达式而存在，而函数是可以的
     // 2. 函数必须要有参数列表，而方法可以没有参数列表
     // 3. 方法名是方法调用，而函数名只是代表函数对象本身
@@ -18,10 +19,24 @@ object FunctionDemo {
       r
     }
 
-    //二、默认参数和带名参数
+    // 二、默认参数和带名参数：可以指定默认参数
     def decorate(str: String, left: String = "[", right: String = "]") = left + str + right
+    // 全部使用默认参数
+    println(decorate("Hello")) // output:[Hello]
+    // 使用部分默认参数
+    println(decorate("Hello", ">>>[")) // output:>>>[Hello]
+    // 全部使用带名参数（可交换顺序）
+    println(decorate(left = "<<<", str = "Hello", right = ">>>")) // output:<<<Hello>>>
+    // 使用部分带名参数
+    println(decorate("Hello", right = "]<<<")) // output:[Hello]<<<
 
-    print(decorate("Hello"))
+    // 三、变长参数
+    def sum(args: Int*) = {
+      var res = 0
+      for (arg <- args) res += arg
+      res
+    }
 
+    println(sum(1, 4, 9, 16, 25)) // output:55
   }
 }
