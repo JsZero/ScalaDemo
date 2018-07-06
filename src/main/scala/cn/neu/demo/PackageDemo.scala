15a7dd3e5a8debea9f7cf8aa93ce71ad3be4e35b
Waitomo <34368246+Waitomo@users.noreply.github.com>
2018-07-06 12:40:06 +0800

    something about Package in Scala

--- src/main/scala/cn/neu/demo/PackageDemo.scala
+++ src/main/scala/cn/neu/demo/PackageDemo.scala
@@ -6,6 +6,8 @@ package com {
 
       class Employee {}
 
+      g
+
     }
 
   }
@@ -58,7 +60,53 @@ package com.horstmann.impatient {
 // ... ...
 // class Person
 // ...
-// 当文件中所有的代码属于同一个包的时候，这是比较好的做法
+// 相当于
+// package com.horstmann.impatient{
+//   package people{
+//     class Person
+//     ...
+//   }
+// }
+// 当文件中所有的代码属于同一个包的时候，这是比较好的做法，
+// people 包内的内容可以直接引用 impatient 包的内容
+
+// 5、包对象
+// 包可以包含类，对象和特质，但是不能包含函数和变量的定义
+// 为了解决这个问题出现了包对象
+package object people {
+  val defaultName = "John Q.Public"
+}
+package people {
+
+  class Person {
+    var name = defaultName // 从包对象拿到的变量
+    // 在本包下访问所以不需要加限定词
+  }
+
+}
+// 6、包可见性
+// 在 Java 中可见性中不写修饰符的话默认是包含该类的包下可见
+// 在 Scala 中需要使用 private[包名] 指定在本包或者上层包可见
+
+// 7、包的引入
+// 类似Java中的引入，但是Scala中引入的通配符是"_"表示所有
+// import java.awt._
+
+// 8、Scala中在任何地方都可以引入包
+
+// 9、重命名和隐藏方法
+// 选取器引入多个成员
+import java.awt.{Color, Font}
+
+import scala.collection.mutable
+// 选取器选取成员并重命名
+// 以下方式我们可以使用JavaHashMap访问Java的hashmap，而用HashMap访问Scala中的HashMap
+// 选取器在进行选取的时候应该把"_"通配符放在最后，否则报错
+import java.util.{HashMap=>JavaHashMap}
+import scala.collection.mutable._
+// =>_ 隐藏某个成员而不是重命名，用来解决冲突问题
+import java.util.{HashMap=>_,_}
+
 class PackageDemo {
   def main(args: Array[String]): Unit = {
     // 1、包
