d859648e4f2f3a365e33e9bce54c7e7683923757
Waitomo <34368246+Waitomo@users.noreply.github.com>
2018-07-09 09:36:00 +0800

    something about Construction order and advance definition in Scala

--- src/main/scala/cn/neu/demo/InheritDemo/ConstructSeq.scala
+++ src/main/scala/cn/neu/demo/InheritDemo/ConstructSeq.scala
@@ -1,5 +1,30 @@
 package cn.neu.demo.InheritDemo
 
-object ConstructSeq {
+class Creature {
+  val range: Int = 10
+  val env: Array[Int] = new Array[Int](range)
+}
 
+class Ant extends Creature {
+  override val range = 2
+}
+
+class Item(val description: String, val price: Double) {
+  final override def equals(other: Any): Boolean = {
+    other.isInstanceOf[Item] && {
+      val that = other.asInstanceOf[Item]
+      description == that.description && price == that.price
+    }
+  }
+}
+
+object ConstructSeq {
+  def main(args: Array[String]): Unit = {
+    val a = new Ant
+    println(a.env.length, a.range)
+    val c = new Creature
+    println(c.env.length)
+    val scores = Map(("Alice", 10), ("Bob", 3), ("Cindy", 8))
+    scores.get("sda")
+  }
 }
